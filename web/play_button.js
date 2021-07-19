48467231deb981c4d6fdbb3fc411874f13f28e83
Ren Tatsumoto <tatsu@autistici.org>
2021-07-19 05:21:48 +0300

    simplify js

--- web/play_button.js
+++ web/play_button.js
@@ -1,34 +1,29 @@
 const BrowserPlayButton = {
-    make_play_button: (ord, display) => {
+    make_play_button: (ord) => {
         const play_button = document.createElement('span')
         play_button.classList.add('play-icon')
         play_button.setAttribute('title', 'play sound')
         play_button.addEventListener('click', () => pycmd(`play_field:${ord}`))
-        if (!display) {
-            play_button.classList.add('hidden')
-        }
         return play_button
     },
     load_icons: () => {
+        // forEditorField is supported by Anki 2.1.41+
         pycmd(`get_fields_with_audio`, (fields) => {
             forEditorField(fields, (field, contains_audio) => {
                 if (field.play_button === undefined) {
-                    const play_button = BrowserPlayButton.make_play_button(field.getAttribute("ord"), contains_audio)
+                    const play_button = BrowserPlayButton.make_play_button(field.getAttribute("ord"))
                     field.labelContainer.insertBefore(play_button, field.label)
                     field.play_button = play_button
-                } else {
-                    field.play_button.classList.toggle('hidden', !contains_audio)
                 }
+                field.play_button.classList.toggle('hidden', !contains_audio)
             })
         })
     },
     hide_icons: () => {
         const fields = document.getElementById("fields")
         if (fields) {
-            for (field of fields.children) {
-                if (field.play_button !== undefined) {
-                    field.play_button.classList.toggle('hidden', true)
-                }
+            for (const icon of fields.getElementsByClassName('play-icon')) {
+                icon.classList.toggle('hidden', true)
             }
         }
     },
