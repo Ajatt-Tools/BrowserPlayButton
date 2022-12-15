62c78ca180cc8be7ad29371146a9ef6349e9a35a
Ren Tatsumoto <tatsu@autistici.org>
2022-12-16 01:47:49 +0300

    null check

--- web/play_button.js
+++ web/play_button.js
@@ -10,14 +10,16 @@ const BrowserPlayButton = {
     load_icons: () => {
         pycmd(`get_fields_with_audio`, (audio_flags) => {
             const label_containers = document.querySelectorAll('.label-container > span:first-child')
-            audio_flags.forEach((contains_audio, i) => {
-                let play_button = label_containers[i].getElementsByClassName(BrowserPlayButton.class_name)[0]
-                if (!play_button) {
-                    play_button = BrowserPlayButton.make_play_button(i)
-                    label_containers[i].prepend(play_button)
-                }
-                play_button.toggleAttribute('hidden', !contains_audio)
-            })
+            if (audio_flags !== null) {
+                audio_flags.forEach((contains_audio, i) => {
+                    let play_button = label_containers[i].getElementsByClassName(BrowserPlayButton.class_name)[0]
+                    if (!play_button) {
+                        play_button = BrowserPlayButton.make_play_button(i)
+                        label_containers[i].prepend(play_button)
+                    }
+                    play_button.toggleAttribute('hidden', !contains_audio)
+                })
+            }
         })
     },
     hide_icons: () => {
