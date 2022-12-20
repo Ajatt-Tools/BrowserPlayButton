d7721ae227ab1014464b22d57bfac4c549afee4c
Ren Tatsumoto <tatsu@autistici.org>
2022-12-21 02:56:05 +0300

    quickfix for anki 2.1.55

--- web/play_button.js
+++ web/play_button.js
@@ -9,7 +9,7 @@ const BrowserPlayButton = {
     },
     load_icons: () => {
         pycmd(`get_fields_with_audio`, (audio_flags) => {
-            const label_containers = document.querySelectorAll('.label-container > span:first-child')
+            const label_containers = document.querySelectorAll('.label-container > span:last-child')
             if (audio_flags !== null) {
                 audio_flags.forEach((contains_audio, i) => {
                     let play_button = label_containers[i].getElementsByClassName(BrowserPlayButton.class_name)[0]
