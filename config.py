09073b0d1c22136357c2ca49ac0ecca85dcdc70a
Ren Tatsumoto <tatsu@autistici.org>
2024-09-17 23:00:56 +0300

    add properties

--- config.py
+++ config.py
@@ -8,5 +8,25 @@ class BrowserPlayButtonConfig(AddonConfigManager):
     def __init__(self, default: bool = False) -> None:
         super().__init__(default)
 
+    @property
+    def show_tooltips(self) -> bool:
+        return bool(self["show_tooltips"])
+
+    @property
+    def show_play_field_action(self) -> bool:
+        return bool(self["show_play_field_action"])
+
+    @property
+    def show_play_selection_action(self) -> bool:
+        return bool(self["show_play_selection_action"])
+
+    @property
+    def show_toolbar_button(self) -> bool:
+        return bool(self["show_toolbar_button"])
+
+    @property
+    def autoplay(self) -> bool:
+        return bool(self["autoplay"])
+
 
 config = BrowserPlayButtonConfig()
