d6d67f864033a699b18c7c87b16118a8cc57dc68
Ren Tatsumoto <tatsu@autistici.org>
2024-09-17 23:07:39 +0300

    use properties in editor

--- editor.py
+++ editor.py
@@ -27,12 +27,12 @@ def play_field(editor: Editor) -> None:
     if editor.currentField is not None:
         field_content = editor.note.fields[editor.currentField]
         play_text(field_content)
-    elif config.get("show_tooltips") is True:
+    elif config.show_tooltips is True:
         tooltip("No field selected.")
 
 
 def append_editor_button(buttons: list[str], editor: Editor) -> None:
-    if config.get("show_toolbar_button") is False:
+    if config.show_toolbar_button is False:
         return
 
     icon_path = os.path.join(get_addon_path(), PLAY_ICON_FILEPATH)
@@ -48,17 +48,17 @@ def append_editor_button(buttons: list[str], editor: Editor) -> None:
 
 
 def add_context_menu_item(webview: EditorWebView, menu: QMenu) -> None:
-    if config.get("show_play_field_action") is True:
+    if config.show_play_field_action is True:
         play_field_action: QAction = menu.addAction("Play field")
         qconnect(play_field_action.triggered, lambda _=False: play_field(webview.editor))
 
-    if config.get("show_play_selection_action") is True:
+    if config.show_play_selection_action is True:
         play_selection_action: QAction = menu.addAction("Play selection")
         qconnect(play_selection_action.triggered, lambda _=False: play_text(webview.editor.web.selectedText()))
 
 
 def on_load_note(editor: Editor) -> None:
-    if config["autoplay"] and editor.note and getattr(editor, "last_note_id__", None) != editor.note.id:
+    if config.autoplay and editor.note and getattr(editor, "last_note_id__", None) != editor.note.id:
         play_text(".".join(editor.note.fields), quiet=True)
         setattr(editor, "last_note_id__", editor.note.id)
 
